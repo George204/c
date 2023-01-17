6581a14908ec98c2360fc0f5067240aa74e5017a
George204 <hubert.lazowy@gmail.com>
2023-01-17 22:35:39 +0100

    a

---
+++ 1.0.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+#include <conio.h>
+using namespace std;
+int main()
+{
+	cout<< "cze��"<<endl<<endl<<endl<<"Na�i�cnij dowolny klawisz";
+	getch();system("cls");
+	cout<< "dowidzenia";
+	return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    cout<<"helo";
+    
+}
