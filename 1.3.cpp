d71bc806ac1245907b3edac79c69b64884c0b3dc
George204 <hubert.lazowy@gmail.com>
2023-01-18 00:25:08 +0100

    zadania c++

---
+++ 1.3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+#include <conio.h>
+#include <cmath>
+#include <unistd.h>
+using namespace std;
+int main()
+{
+	cout<< "czeœæ";
+	sleep(5);system("cls");
+	float polepow = pow(5.875,2)*6;
+	float obj = pow(5.875,3);
+	cout<<fixed<<setprecision(2)<<"pole pow.:"<<polepow<<endl<<"objêtoœæ:"<<obj;
+	getch();
+	return 0;
+}
