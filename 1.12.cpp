48c2ec3b1bf475a2d9bffb7f9c6d41da1a6c6e02
George204 <hubert.lazowy@gmail.com>
2023-03-13 23:48:52 +0100

    A
    A

---
+++ 1.12.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+#include <conio.h>
+#include <cmath>
+#include <unistd.h>
+#include <locale.h>
+#define ndk cout<<"\nnaciï¿½nij dowolny klawisz";getch();cout<<"\r                                      \r\n";
+
+using namespace std;
+int main()
+{
+	setlocale(LC_CTYPE, "Polish");
+	while(true){
+	float a,b,c,d;
+	cout<<fixed<<setprecision(2)<<"równanie ax^2+bx+c=d"<<endl;
+	cout<<"a:";	cin>>a;
+	cout<<"b:"; cin>>b;
+	cout<<"c:";	cin>>c;
+	cout<<"d:";	cin>>d;
+//	system("cls");
+//	cout<<"obliczanie dla równania"<<a<<"x^2+"<<b<<"x+"<<c<"="<<d;
+	if(a==0){
+		if(b==0){
+			if(c==d){cout<<"równanie tozszamoœciowe";}
+			else if (c!=d){cout<<"równanie sprzeczne";}}
+		else{cout<<"x="<<((d-c)/b);}}
+	else{
+		c = c-d;
+		float delta = pow(b,2)-(4*a*c);
+		if(delta == 0){
+			float x = (-1*b)/(2*a);
+			cout<<"x:"<<x;
+		}
+		else if(delta > 0){
+			float x1 = (-b-sqrt(delta))/(2*a);
+			float x2 = (-b+sqrt(delta))/(2*a);
+			cout<<"x1:"<<x1<<"\nx2:"<<x2; }
+		else if (delta < 0){cout<<"brak rozwi¹zañ";}}
+	ndk;}
+	return 0;
+}
+
